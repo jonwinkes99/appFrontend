8c0438fe9dc27ef0ac4c671cb2504693fe0900f9
jonwinkles99 <jwinks18@gmail.com>
2020-04-13 18:47:11 -0700

    new

---
+++ src/components/Navbar.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import MobilRightMenuSlider from '@material-ui/core/Drawer'
+import {
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box,
+    ListItemIcon,
+    Drawer
+} from "@material-ui/core"
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    Apps,
+    ContactMail
+} from "@material-ui/icons"
+import avatar from '../jonpic.jpg'
+
+// CSS styles
+const useStyles = makeStyles(theme=>({
+    menuSliderContainer: {
+        width: 250,
+        background: "#494B64",
+        height:"480px"
+    },
+    avatar: {
+        display: "block",
+        margin: "8px auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13)
+    },
+    listItem: {
+        color: "tan"
+    }
+}));
+
+const menuItems = [
+    {
+        listIcon: <Home/>,
+        listText: "Home"
+    },
+    {
+        listIcon: <AssignmentInd/>,
+        listText: "Resume"
+    },
+    {
+        listIcon: <ContactMail/>,
+        listText: "Contacts"
+    }
+]
+
+const Navbar = () => {
+    const [state, setState] = useState({
+        right: false
+    })
+    const toggleSlider = (slider, open) => () => {
+        setState({...state, [slider]: open })
+    }
+    const classes = useStyles()
+    const sideList = slider => (
+        <Box className={classes.menuSliderContainer} component="div">
+        <Avatar className={classes.avatar} src={avatar} alt="Jonathan Winkles"/>
+        <Divider/>
+        <List>
+            {menuItems.map((lsItem, key)=>(
+                <ListItem button key={key}>
+                    <ListItemIcon className={classes.listItem}>{lsItem.listIcon}</ListItemIcon>
+                    <ListItemText className={classes.listItem} primary={lsItem.listText}/>
+                </ListItem>
+            ))}
+            
+        </List> 
+        </Box>  
+    )
+    return (
+        <>
+        
+        <Box component="nav">
+          <AppBar position="static" style={{background: "#111"}}>
+              <Toolbar>
+                <IconButton>
+                  <ArrowBack style={{color: "#4E538F"}}/>
+                </IconButton>
+                <Typography variant="h5">
+                    Jon Winkles
+                </Typography>
+              </Toolbar>
+            </AppBar>  
+        </Box>
+        </>
+    )
+}
+
+export default Navbar
+ 
